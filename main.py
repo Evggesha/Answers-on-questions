6f41ef9b8e28ca91a5698fcf92557627810a982f
Женя <golovatow@mail.ru>
2022-08-13 13:54:45 +0300

    Answers on questions

---
+++ main.py
@@ -0,0 +1,25 @@
+# str
+name = "Hello"
+print(name)
+name1 = "Салам-пополам"
+print(name1)
+#bool
+talking = "True"
+print(talking)
+liar = "False"
+print(liar)
+#int
+date = 1997
+print(date)
+tomorrow = 1408
+print(tomorrow)
+today = 1308
+print(today)
+#float
+price = 2000.500
+print(price)
+christmas = 31.12
+print(christmas)
+growth = 187.6
+print(growth)
+#none
