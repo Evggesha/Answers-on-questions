b56108b09fcbacbc50bfc05487ca159f4475c01d
Женя <golovatow@mail.ru>
2022-08-15 13:10:01 +0300

    Числа

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-str1 = "Hello"
-str2 = "123"
-print(type(str2))
-str3 = int(str2)
-print(type(str3))
+print(1**100)
+print(2**16)
+print(2-1000)
+print(2+2*2)
+print(60/3)
+
+import math
+print(math.sqrt(144))
+x = 72
+print(2+5*14)
+x = 55
+print(66-x==11)
+x = 64
+print(math.sqrt(x)+8)
+
